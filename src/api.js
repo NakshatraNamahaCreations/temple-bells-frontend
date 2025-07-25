3f96fb214f8b157be1d619504bd20cef9db67c38
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2025-07-25 17:12:22 +0530

    server link updated

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-// export const ApiURL = "https://api.theweddingrentals.in/api";
+export const ApiURL = "https://api.theweddingrentals.in/api";
 export const ImageApiURL = "https://api.theweddingrentals.in";
 
 // export const ApiURL = "http://192.168.1.199:8077/api";
 // export const ImageApiURL = "http://192.168.1.199:8077";
 
-export const ApiURL = "http://localhost:8077/api";
+// export const ApiURL = "http://localhost:8077/api";
 // export const ImageApiURL = "http://localhost:8077";
